edce1efd099d487f804a80218feb8dcde57e8b05
hiteple <hiteplegold@gmail.com>
2020-11-28 19:44:17 -0300

    Initial commit :tada:

---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const otherWordsForHome = require('../assets/images/other_words_for_home.jng');
+export const theMetropolist = require('../assets/images/the_metropolist.jng');
+export const theTinyDragon = require('../assets/images/the_tiny_dragon.jng');
+export const underland = require('../assets/images/underland.jng');
+
+export default {
+   otherWordsForHome,
+   theMetropolist,
+   theTinyDragon,
+   underland
+}
